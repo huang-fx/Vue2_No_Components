1747aaf79001a0f4dba39ffdbed214a990614f04
GirdvainisKostas@gmail.comgit config --global user.name GirdvainisKostas@gmail.com <GirdvainisKostas@gmail.com>
2026-05-09 17:59:07 +0800

    提交 Vue2 学习项目

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: '尚硅谷',
+                address: '沈阳'
+            }
+        },
+    }
+</script>
+
+<style>
+    .demo {
+        background-color: burlywood;
+    }
+</style>
